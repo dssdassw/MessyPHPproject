bc03bf4f047b4b48365085246d3321e564de7750
school <Eric.Sartor.024@students.hcdsb.org>
2016-06-06 13:08:41 -0400

    Please kill me

--- greenhouse_action.php
+++ greenhouse_action.php
@@ -4,34 +4,34 @@
 	</head>
 	<body>
 		<?php
-			$names = array('main_tank_temp',   //sensor 1  - T001
-						   'main_tank_rpot',   //sensor 2  - O001
-						   //sensor 3 omitted
-						   'main_tank_o2',     //sensor 4  - D001 
-						   'main_tank_pHa',    //sensor 5  - P001
-						   //sensor 6 omitted
-						   'z3_ground',        //sensor 7  - T010
-						   'z3_middle',        //sensor 8  - T007
-						   'z3_ceiling',       //sensor 9  - T006
-						   'small_tank_temp',  //sensor 10 - T011
-						   'z2_ceiling',       //sensor 11 - T003
-						   'z2_middle',        //sensor 12 - T008
-						   'z1_ceiling',       //sensor 13 - T012
-						   'z1_ground',        //sensor 14 - T005
-						   //sensor 15 omitted
-						   'main_tank_pHb',    //sensor 16 - PE01
-						   'main_tank_pHc',    //sensor 17 - PE02
-						   'main_tank_pHd',    //sensor 18 - PE03
-						   'main_tank_bottom', //sensor 19 - T020
-						   'main_tank_middle', //sensor 20 - T021
-						   'main_tank_top'     //sensor 21 - T022
+			$names = array(0 => 'main_tank_temp',   //sensor 1  - T001
+						   1 => 'main_tank_rpot',   //sensor 2  - O001
+													//sensor 3 omitted
+						   2 => 'main_tank_o2',		//sensor 4  - D001 
+						   3 => 'main_tank_pHa',	//sensor 5  - P001
+													//sensor 6 omitted
+						   4 => 'z3_ground',		//sensor 7  - T010
+						   5 => 'z3_middle',		//sensor 8  - T007
+						   6 => 'z3_ceiling',	    //sensor 9  - T006
+						   7 => 'small_tank_temp',  //sensor 10 - T011
+						   8 => 'z2_ceiling',	    //sensor 11 - T003
+						   9 => 'z2_middle',		//sensor 12 - T008
+						   10 => 'z1_ceiling',		//sensor 13 - T012
+						   11 => 'z1_ground',		//sensor 14 - T005
+													//sensor 15 omitted
+						   12 => 'main_tank_pHb',			//sensor 16 - PE01
+						   13 => 'main_tank_pHc',			//sensor 17 - PE02
+						   14 => 'main_tank_pHd',			//sensor 18 - PE03
+						   15 => 'main_tank_bottom', 		//sensor 19 - T020
+						   16 => 'main_tank_middle', 		//sensor 20 - T021
+						   17 => 'main_tank_top'			//sensor 21 - T022
 			);
 			$senid = array('T001',
 						   'O001',
-						   NULL,
+						   //NULL,
 						   'D001',
 						   'P001',
-						   NULL,
+						   //NULL,
 						   'T010',
 						   'T007',
 						   'T006',
@@ -40,7 +40,7 @@
 						   'T008',
 						   'T012',
 						   'T005',
-						   NULL,
+						   //NULL,
 						   'PE01',
 						   'PE02',
 						   'PE03',
@@ -48,8 +48,17 @@
 						   'T021',
 						   'T022'
 			);
-			foreach ($checkboxes as $val){
-				print isset($val);
+			print "Checkbox read results: ";
+			print ($names[3]);
+			for ($i = 0; $i < 18; $i++) {
+				$temp = $_POST[($names[$i]);
+				print "</br>" . $i . ". " . ($temp) . ": " . isset($val);
+				if ($temp == 'on') {
+					print " DETECTED!";
+				}
+				else {
+					print " Not active.";
+				}
 			}
 		?>
 	</body>
